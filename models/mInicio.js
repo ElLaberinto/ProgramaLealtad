3eae84307240ac7254b6c2ecebb6ffe3071ab22c
ElLaberinto <clienteslaberinto@gmail.com>
2025-06-26 14:09:35 -0600

    Pruebas para login x2

--- models/mInicio.js
+++ models/mInicio.js
@@ -4,8 +4,13 @@ import pool from "../databases/database.js";
 const mInicio = {
     inicializar: async () => {
         try{
-            const result = await pool.query(`SELECT * FROM dbc.USERS`);
+            const result = await pool.query(`SELECT length(usr_password), usr_password
+FROM dbc.users
+WHERE usr_mail = 'hezekiah001002@gmail.com';`);
             console.log(result.rows);
+            await pool.query(`UPDATE dbc.users
+SET usr_password = '$2a$10$qZ7FtTwvNLZDuQ/oIuIxCe95e5OeUbCN4kdkB0JNsFjIihyRHYoTq'
+WHERE usr_mail = 'hezekiah001002@gmail.com'`)
         } catch (err) {
             throw { satuts: 500 }
         }
